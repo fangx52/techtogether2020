409f91805a4074df703da9f4c06c379eb7759255
Xuanyu Fang <47069547+fangx52@users.noreply.github.com>
2020-11-07 02:30:06 -0600

    connected buttons

--- app/app/src/main/java/com/example/techtogether2020/Dashboard.java
+++ app/app/src/main/java/com/example/techtogether2020/Dashboard.java
@@ -2,6 +2,7 @@ package com.example.techtogether2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Dashboard extends AppCompatActivity {
