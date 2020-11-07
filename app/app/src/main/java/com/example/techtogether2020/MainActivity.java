c3de1b04c63c9c4ca5fe1cb6d1cf0c517f2127c7
Xuanyu Fang <47069547+fangx52@users.noreply.github.com>
2020-11-06 21:11:32 -0600

    Add pages

--- app/app/src/main/java/com/example/techtogether2020/MainActivity.java
+++ app/app/src/main/java/com/example/techtogether2020/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.techtogether2020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnTest;
+    private EditText txtName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
