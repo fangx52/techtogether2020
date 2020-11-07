771ec8843dcafda94ea66d93056bb7b855686864
Xuanyu Fang <47069547+fangx52@users.noreply.github.com>
2020-11-07 14:24:33 -0600

    cleanup of signin and signup

--- app/app/src/main/java/com/example/techtogether2020/Dashboard.java
+++ app/app/src/main/java/com/example/techtogether2020/Dashboard.java
@@ -4,12 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Dashboard extends AppCompatActivity {
-
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+
+        String email = user.getEmail();
+        Log.i("UserInfo", email);
+
     }
 }
