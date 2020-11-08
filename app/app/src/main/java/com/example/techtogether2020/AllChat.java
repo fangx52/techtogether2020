623d62641b1b252de6563ee2d17cb2ae3ff4c492
Xuanyu Fang <47069547+fangx52@users.noreply.github.com>
2020-11-08 10:21:56 -0600

    clean up and connect allchat to chat

--- app/app/src/main/java/com/example/techtogether2020/AllChat.java
+++ app/app/src/main/java/com/example/techtogether2020/AllChat.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class AllChat extends AppCompatActivity {
     private TextView txtVwName;
     private String ID;
     private String groupName;
+    private Button btnAllMemebers, btnTeam1, btnTeam2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +24,38 @@ public class AllChat extends AppCompatActivity {
         groupName = intent.getStringExtra("Group");
         txtVwName = (TextView) findViewById(R.id.txtVwName);
         txtVwName.setText(groupName);
+        btnAllMemebers=(Button) findViewById(R.id.btnAllMembers);
+        btnTeam1=(Button) findViewById(R.id.btnTeam1);
+        btnTeam2 = (Button) findViewById(R.id.btnTeam2);
+        btnAllMemebers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavigatetoChat("All Members");
+
+            }
+        });
+        btnTeam1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavigatetoChat("Team1");
+            }
+        });
+        btnTeam2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavigatetoChat("Team2");
+            }
+        });
 
     }
-
+    public void NavigatetoChat(String channel){
+        Intent intent = new Intent(AllChat.this, Chat.class);
+        intent.putExtra("Chat", ID);
+        intent.putExtra("Group", groupName);
+        intent.putExtra("Channel", channel);
+        startActivity(intent);
+        finish();
+    }
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
