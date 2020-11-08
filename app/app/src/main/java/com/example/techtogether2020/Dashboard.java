c3607bd3f9a4f0a9db072198cb3736903f65c70f
ruby881999 <erinqiu@bu.edu>
2020-11-08 00:53:18 -0500

    Create Group

--- app/app/src/main/java/com/example/techtogether2020/Dashboard.java
+++ app/app/src/main/java/com/example/techtogether2020/Dashboard.java
@@ -4,23 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class Dashboard extends AppCompatActivity {
-    private FirebaseAuth auth;
+    private ImageButton imgBtnAddGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        auth = FirebaseAuth.getInstance();
-        FirebaseUser user = auth.getCurrentUser();
-
-        String email = user.getEmail();
-        Log.i("UserInfo", email);
+        imgBtnAddGroup = (ImageButton) findViewById(R.id.imgBtnAddGroup);
+        imgBtnAddGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Dashboard.this,CreateGroup.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
