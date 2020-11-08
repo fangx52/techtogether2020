623333dc1f86fef99cd749da7fb386f3b02fe8fc
Lulu Yu <62454205+lulu-yu@users.noreply.github.com>
2020-11-08 07:59:05 -0800

    fixed bug

--- app/app/src/main/java/com/example/techtogether2020/Chat.java
+++ app/app/src/main/java/com/example/techtogether2020/Chat.java
@@ -12,18 +12,24 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class Chat extends AppCompatActivity {
     FirebaseAuth auth;
+    private String ID;
+    private String groupName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-
+        Intent intent = getIntent();
+        ID = intent.getStringExtra("ID");
+        groupName = intent.getStringExtra("Group");
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
             Intent intent = new Intent(Chat.this, AllChat.class);
+            intent.putExtra("Chat", ID);
+            intent.putExtra("Group", groupName);
             startActivity(intent);
             finish();
             return true;
